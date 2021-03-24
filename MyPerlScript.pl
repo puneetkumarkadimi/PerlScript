91c0eb60ea2e52efd89f182ba60ef675d48196eb
puneet kumar kadimi <puneetkkadimi@gmail.com>
2021-03-24 10:31:44 +0530

    Today created 4D array and added the dumper functionality in it.

---
+++ MyPerlScript.pl
@@ -0,0 +1,23 @@
+use warnings;
+use strict;
+use Data::Dumper;
+
+my @array;
+
+for(my $i=0; $i < 10; $i++)
+{
+	for(my $j=0; $j < 10; $j++)
+	{
+		for(my $x=0; $x < 10; $x++)
+		{
+			for(my $y=0; $y < 10; $y++)
+			{
+				$array[$i][$j][$x][$y]="$i,$j,$x,$y";
+			}
+		}
+	}
+}
+
+print Dumper(\@array);
+
+exit;
